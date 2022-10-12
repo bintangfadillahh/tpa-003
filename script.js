e8e409fefa01afafe9c234e6fd7a5dbbe3a89559
star2112 <bintangfadel73@gmail.com>
2022-10-12 12:52:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let row = document.getElementById("item");
+
+let card = `<div class="col-xxl-6 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12">
+<div class="card" style="width: 18rem">
+<div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">
+    Some quick example text to build on the card title and make up
+    the bulk of the card's content.
+    </p>
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div>`;
